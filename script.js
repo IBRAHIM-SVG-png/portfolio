5a05c7703345792efcea5d75ec2eb0df5887332c
ibrahim-dev <ibrahimj@karunya.edu.in>
2026-03-11 14:58:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-function addHabit(){
+let cells = document.querySelectorAll(".cell");
+let player = "X";
+let gameOver = false;
 
-let input=document.getElementById("habitInput");
-let list=document.getElementById("habitList");
+const winPatterns = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+];
 
-if(input.value==="") return;
+cells.forEach((cell, index) => {
+cell.addEventListener("click", () => {
 
-let li=document.createElement("li");
-li.textContent=input.value;
+if(cell.textContent !== "" || gameOver) return;
 
-list.appendChild(li);
+cell.textContent = player;
 
-input.value="";
+checkWinner();
+
+player = player === "X" ? "O" : "X";
+
+});
+});
+
+function checkWinner(){
+
+winPatterns.forEach(pattern => {
+
+let a = cells[pattern[0]].textContent;
+let b = cells[pattern[1]].textContent;
+let c = cells[pattern[2]].textContent;
+
+if(a !== "" && a === b && b === c){
+
+alert(a + " Wins!");
+gameOver = true;
+
+}
+
+});
+
 }
