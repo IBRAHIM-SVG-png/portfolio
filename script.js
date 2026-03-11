b80af9eee7300b06785978caf82a6c31560be75a
ibrahim-dev <ibrahimj@karunya.edu.in>
2026-03-11 14:58:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-let cells = document.querySelectorAll(".cell");
-let player = "X";
-let gameOver = false;
+async function getWeather(){
 
-const winPatterns = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]
-];
+let city=document.getElementById("city").value;
 
-cells.forEach((cell, index) => {
-cell.addEventListener("click", () => {
+let apiKey="YOUR_API_KEY";
 
-if(cell.textContent !== "" || gameOver) return;
+let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-cell.textContent = player;
+try{
 
-checkWinner();
+let response=await fetch(url);
+let data=await response.json();
 
-player = player === "X" ? "O" : "X";
-
-});
-});
-
-function checkWinner(){
-
-winPatterns.forEach(pattern => {
-
-let a = cells[pattern[0]].textContent;
-let b = cells[pattern[1]].textContent;
-let c = cells[pattern[2]].textContent;
-
-if(a !== "" && a === b && b === c){
-
-alert(a + " Wins!");
-gameOver = true;
+document.getElementById("cityName").innerText=data.name;
+document.getElementById("temperature").innerText="Temperature: "+data.main.temp+" °C";
+document.getElementById("description").innerText="Weather: "+data.weather[0].description;
 
 }
+catch(error){
 
-});
+alert("City not found");
+
+}
 
 }
